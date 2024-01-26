5e6e507db05e14b7a86504f05b350dcfbb67f913
leebohm <102368451+leebohm@users.noreply.github.com>
2024-01-26 15:25:14 +0900

    Title: 원소 값들의 최대 합, Time: 15ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240126/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int n,m;
+int arr[101];
+using namespace std;
+
+int main() {
+
+    cin >> n >> m;
+
+    for(int i=1; i<=n;i++)
+        cin >> arr[i];
+
+    int ans =0;
+    
+    for(int i=1; i<=m;i++){ // 움직임
+        int tmp_ans = 0;
+        int num = i;
+        for(int j=1; j<=n;j++){ // 시작위치
+            int temp = arr[j];
+
+            while(num){
+                tmp_ans += temp;
+                temp = arr[temp];
+                num--;
+
+            }
+            ans = max(ans, tmp_ans);
+
+        }
+
+    }
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
