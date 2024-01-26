9126c036e03a3f553b62717d3ff7b5a544e3ed57
leebohm <102368451+leebohm@users.noreply.github.com>
2024-01-26 16:00:50 +0900

    Title: 독서실의 거리두기 5, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240126/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240126/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -1,46 +1,35 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int n;
-string str;
-int arr[20];
+string seat;
+
+int MinDist(){
+    int min_dist = n;
+
+    for(int i=0; i<n;j++)
+        for(int j=i+1;j<n;j++)
+            if(seat[i]=='1' && seat[j]=='1')
+                min_dist = min(min_dist,j-i);
+    
+    return min_dist;
+}
 
 int main() {
-
     cin >> n;
-    cin >> str;
+    cin >> seat;
 
-    for(int i=0; i<n;i++)
-        arr[i] = str[i]-'0';
     int ans =0;
-
-    for(int i=0; i<n;i++){ // x 를 놓을 위치
-        int temp[n] = {};
-        for(int j=0; j<n;j++) //초기화
-            temp[j] = arr[j];
-
-       
-
-        if(temp[i]!=1) temp[i] = 1; // x 배치
-        else continue;
-
-        //빈자리 최솟값 구하기
-        int cnt = 0;
-        int min_cnt = 1e9;
-        
-        for(int k=0;k<n;k++){
-            if(temp[k]==0) cnt++;
-            else {
-                if(cnt!=0) min_cnt = min(min_cnt,cnt);
-                cnt =0;}
+    for(int i=0; i<n;i++){
+        if(seat[i]=='0'){
+            seat[i] ='1';
+            ans = max(ans, MinDist());
+            seat[i] = '0';
         }
-
-        ans = max(min_cnt,ans);
     }
-    
     cout << ans;
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
