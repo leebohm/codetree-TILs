38b0ba9115fca4c16b58d203e02bfd56081e0223
leebohm <102368451+leebohm@users.noreply.github.com>
2024-01-30 14:24:36 +0900

    Title: 독서실의 거리두기 4, Time: 30ms, Memory: 0MB, Status: Passed - CodeTree

--- 240130/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
+++ 240130/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -1,55 +1,91 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// string str;
+// int n;
+
+// int main() {
+
+//     cin >> n;
+//     cin >> str;
+    
+//     int ans =0;
+//     for(int i=0; i<n-1;i++)
+//         for(int j=i+1; j<n;j++){
+//             string temp = str;
+//             if(temp[i]!='1' && temp[j]!='1'){
+//                 temp[i]='1', temp[j]='1';
+
+//                 int num = 1; 
+//                 int ch = 1e9;
+//                 bool flag = true;
+//                 for(int k=0; k<n;k++){
+//                     if(temp[k]=='0') num++;
+//                     else if(temp[k]=='1'){
+//                         if(k==0) continue;
+//                         else if(num!=1) {
+//                             ch=min(ch,num);
+//                             num =1;
+//                         }
+//                         else if(num == 1){
+//                             ch=min(ch,num);
+//                         }
+//                         else if(temp[k-1]=='1') {
+//                             flag = false;
+//                             break;
+//                             ch=0;
+//                             }
+                         
+                        
+//                     }
+//                 }
+//                 if(flag) {
+//                     ans = max(ch,ans);
+//                     }
+//             }
+//             else continue;
+            
+//         }
+    
+//     cout << ans;
+//     // 여기에 코드를 작성해주세요.
+//     return 0;
+// }
+
+
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-string str;
 int n;
+string seat;
 
-int main() {
+int MinDist() {
+    int min_dist = n;
+    for(int i=0; i<n;i++)
+        for(int j=i+1; j<n;j++)
+            if(seat[i] =='1' && seat[j] =='1')
+                min_dist = min(min_dist,j-i);
+    
+    return min_dist;
+}
 
+int main(){
     cin >> n;
-    cin >> str;
-    
+    cin >> seat;
     int ans =0;
-    for(int i=0; i<n-1;i++)
-        for(int j=i+1; j<n;j++){
-            string temp = str;
-            if(temp[i]!='1' && temp[j]!='1'){
-                temp[i]='1', temp[j]='1';
 
-                int num = 1; 
-                int ch = 1e9;
-                bool flag = true;
-                for(int k=0; k<n;k++){
-                    if(temp[k]=='0') num++;
-                    else if(temp[k]=='1'){
-                        if(k==0) continue;
-                        else if(num!=1) {
-                            ch=min(ch,num);
-                            num =1;
-                        }
-                        else if(num == 1){
-                            ch=min(ch,num);
-                        }
-                        else if(temp[k-1]=='1') {
-                            flag = false;
-                            break;
-                            ch=0;
-                            }
-                         
-                        
-                    }
-                }
-                if(flag) {
-                    ans = max(ch,ans);
-                    }
+    for(int i=0;i<n;i++)
+        for(int j=i+1;j<n;j++)
+            if(seat[i] == '0' && seat[j] =='0'){
+                seat[i] = seat[j] = '1';
+                ans = max(ans, MinDist());
+                seat[i] = seat[j] = '0';
             }
-            else continue;
-            
-        }
-    
     cout << ans;
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
