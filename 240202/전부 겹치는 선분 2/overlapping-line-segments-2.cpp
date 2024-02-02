369313a26425225b10fd89c41bcb46ad692d0403
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-02 22:19:12 +0900

    Title: 전부 겹치는 선분 2, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240202/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 240202/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -14,17 +14,20 @@ int main() {
     for(int i=0; i<n;i++)
         cin >> x1[i] >> x2[i];
 
-    int min_x2 = INT_MAX;
-    int max_x1 = 0;
+    
     bool flag = false;
     for(int i=0; i<n;i++){ // 이 선을 사용하지 않을 예정
+        int min_x2 = INT_MAX;
+        int max_x1 = 0;
         for(int j=0; j<n; j++){
             if(i!=j){
                 min_x2 = min(min_x2,x2[j]);
                 max_x1 = max(max_x1,x1[j]);
             }
         }
-        if(min_x2 >= max_x1) flag = true;
+        if(min_x2 >= max_x1) {
+            flag = true;
+            }
         if(flag) break;
     }
     if(flag) cout << "Yes";
