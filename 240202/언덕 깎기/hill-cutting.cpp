f73574739a19067d82982a1b8c8a9ccab3feb82b
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-02 20:17:50 +0900

    Title: 언덕 깎기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240202/언덕 깎기/hill-cutting.cpp
+++ 240202/언덕 깎기/hill-cutting.cpp
@@ -1,49 +1,38 @@
 #include <iostream>
+#include <climits>
+
+#define MAX_N 1000
+#defien MAX_H 100
 
 using namespace std;
 
-int n, x;
-int arr[1000];
-
-// 들어가는 최소 비용. 비용을 기록해야함.. 
-// 최대 최소 높이차가 17이하가 되도록.
-// 언덕 높이를 1부터 100까지로 가정하고 해야겠다. 
-// 그러면 그 언덕 높이인게 적어도 한개는 잇어야하는거 아닌가. 
-// 흠........ 
+int n;
+int k=17;
+int arr[MAX_N];
 
 int main() {
-
     cin >> n;
-    for(int i=0; i<n;i++)
+    for(int i=0; i<n; i++)
         cin >> arr[i];
+    
+    int ans = INT_MAX;
 
-    int ans = 1e9;
-    for(int i=0; i<n;i++){ // 설정한 언덕의 최대 높이 
-        int cost = 0;
-        for(int j=0;j<n;j++){
-            if(i!= j){
-                if((arr[i]-arr[j] <= 17) && (arr[i]-arr[j]>=0)){
-                    continue;
-                }
-                else{
-                    if(arr[i]<arr[j]){
-                        int tmp = abs(arr[j] - arr[i]);
-                        cost += tmp*tmp;  
-                    }
-                    else if ((arr[i]>arr[j])&&(abs(arr[i]-arr[j])>17)){
-                        int tmp = abs(arr[j] - abs(arr[i]-17));
-                        cost += tmp*tmp;  
-                    }
-                }
-            
-            }
-
+    for(int i=0; i<= MAX_H; i++){
+        int cost =0;
+        for(int j=0; j<n;j++){
+            if(arr[j]<i)
+                cost += (arr[j]-i) * (arr[j]-i);
+            if(arr[j] > i+k)
+                cost += (arr[j]-i-k) * (arr[j] - i -k);
         }
-        ans = min(cost, ans);
-        
-}    
 
-    cout << ans ;
-    // 여기에 코드를 작성해주세요.
+        ans = min(ans, cost);
+    }
+
+    cout << ans;
+
     return 0;
 }
+
+// i보다 낮은 언덕은 높이가 i 가 되게 쌓으면 되고, 
+// i+k 보다 높은 언덕은 i+k 가 되게 깎으면 되는데
