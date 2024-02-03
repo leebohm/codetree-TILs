74daab0acd516de5865a73cd8f7150dd9d835d60
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-03 21:38:38 +0900

    Title: X 달리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/X 달리기/x-run.cpp
+++ 240203/X 달리기/x-run.cpp
@@ -17,18 +17,21 @@ int main() {
         cur_dist += v;
 
         if((x-cur_dist)>=((v+1)*(v+2))/2){
-            //cout << "case 1 : " << cur_dist << " v : " << v << endl;
             v+=1;
+            //cout << "case 1 : " << cur_dist << " v : " << v << endl;
         }
-        else if((x-cur_dist)<=((v-1)*(v))/2){
-            //cout << "case 2 : " << cur_dist << " v : " << v << endl;
-            v-=1;            
-        } 
-        else if((x-cur_dist)==((v)*(v))/2){
-            //cout << "case 3 : " << cur_dist << " v : " << v << endl;
+        else if((x-cur_dist)>=((v)*(v+1))/2){
             v=v;
+            //cout << "case 2 : " << cur_dist << " v : " << v << endl;
         } 
+        else //if((x-cur_dist)==((v-1)*(v))/2){
+           {
+            v-=1;            
+            //cout << "case 3 : " << cur_dist << " v : " << v << endl;
+        } 
+        
         sec++;
+        //cout << "sec : " << sec <<" "<< cur_dist << " v : " << v << endl;
     }
     cout << sec;
 
