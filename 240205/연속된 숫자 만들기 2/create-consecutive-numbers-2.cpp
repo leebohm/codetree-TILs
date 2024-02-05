a3a20db9e2415c30f24413537af6b5a31f28c454
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-05 21:09:02 +0900

    Title: 연속된 숫자 만들기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240205/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240205/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -1,68 +1,28 @@
 #include <iostream>
 #include <algorithm>
 
-using namespace std;
-int arr[3];
+#define MAX_N 3
 
-int cnt = 0;
+usingnamespace std;
 
-void Case1 () { // 왼쪽, 가운데 사이가 3이상인 경우
-    int tmp1 = arr[1]; // 오른쪽을 왼쪽 +2 로 이동시킴 -> 오른쪽이 가운데가됨
-    arr[1] = arr[0]+2;
-    arr[2] = tmp1; // 가운데가 오른쪽이 됨.  
-
-    cnt++;
-
-    // 그럼 0 0+2 ? 상태가 됨. 맨 오른쪽을 0+1 로 이동시키면 끝
-    int tmp2 = arr[1];
-    arr[1] = arr[0]+1;
-    arr[2] = tmp2;
-
-    cnt++;
-}
-
-void Case2() {  // 가운데, 오른쪽 사이가 3이상인 경우 
-    int tmp1 = arr[1];  // 왼쪽을 가운데 +2로 이동시킴 -> 왼쪽이 가운데가 됨
-    arr[1] = arr[1]+2; 
-    arr[0] = tmp1; // 가운데가 왼쪽이 됨
-    cnt++;
-
-    int tmp2 = arr[1];
-    arr[1] = arr[0]+1;
-    arr[2] = tmp2;
-    cnt++;
-    
-}
+int n=3;
+int a[MAX_N];
 
 int main() {
-
-    for(int i=0; i<3;i++)
-        cin >> arr[i];
     
-    sort(arr, arr+3);
-
-
-    while(arr[1]-arr[0]!=1 && arr[2]-arr[1]!=1){
-        if (arr[1]-arr[0]==2) {
-            cnt++;
-            int tmp = arr[1];
-            arr[1] = arr[0]+1;
-            arr[2] = tmp;}
-
-        else if (arr[2]-arr[1]==2) {
-            cnt++;
-            int tmp = arr[1];
-            arr[1] = arr[1]+1;
-            arr[0] = tmp;
-        }
-        else if(arr[1]- arr[0]>= 3) Case1();
-
-        else if(arr[2] - arr[1]>=3) Case2();
-
-    }
-
-    cout << cnt;
+    for(int i=0; i <n;i++)
+        cin >> a[i];
     
-    // 여기에 코드를 작성해주세요.
+    sort(a,a+n);
+
+    if(a[0]+1 == a[1] && a[1] +1 a[2])
+        cout << 0;
+
+    else if (a[0] + 2== a[1] || a[1] + 2 == a[2])
+        cout << 1;
+    
+    else 
+        cout << 2;
+
     return 0;
 }
