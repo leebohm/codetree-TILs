62e0c73405dbfae851860339a50789da2c2b680e
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-05 16:43:46 +0900

    Title: 연속된 숫자 만들기 2, Time: 1057ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240205/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240205/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -44,7 +44,7 @@ int main() {
 
     while(arr[1]-arr[0]!=1 && arr[2]-arr[1]!=1){
         if (arr[1]-arr[0]==2) cnt++;
-        else if (arr[2]-arr[2]==2) cnt++;
+        else if (arr[2]-arr[1]==2) cnt++;
         else if(arr[1]- arr[0]>= 3) Case1();
 
         else if(arr[2] - arr[1]>=3) Case2();
