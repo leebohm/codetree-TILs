f900225a0b2af8a3f18890704cbc5781cb4bbf35
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-05 22:29:00 +0900

    Title: 독서실의 거리두기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240205/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string str;
+int fin_i,fin_j;
+
+void findMax(){
+    int dist = 0;
+    for(int i=0; i<n;i++){
+        if(str[i]=='1'){
+        for(int j=i+1; j<=n;j++){
+            if(str[j]=='1'){
+                dist = max(dist, j-i);
+                if(dist == j-i){
+                    fin_i = i; fin_j = j;
+                }
+                break;
+            }
+        }
+    }
+    }
+}
+
+int findMin(){
+    int dist = 1e9;
+    for(int i=0; i<n;i++){
+        for(int j=i+1; j<=n;j++){
+            if(str[i] == '1' && str[j]=='1'){
+                dist = min(dist, j-i);
+                break;
+            }
+
+        }
+    }
+    return dist;
+}
+
+int main() {
+
+    cin >> n;
+    cin >> str;
+
+    findMax();
+    str[(fin_j-fin_i)/2+fin_i] = '1';
+    int ans =0;
+    ans = findMin();
+
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
