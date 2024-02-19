adc74673898ce95c2c7969dee82dd654c0b8ddcf
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-19 17:49:17 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240219/숫자의 증감/increasing-and-decreasing-numbers.cpp
+++ 240219/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -13,7 +13,7 @@ int main() {
             cout << i <<" " ;
     } 
     else if(c=='D'){
-        for(int i=n;i=1;i--)
+        for(int i=n;i>=1;i--)
             cout << i << " ";
     }
     // 여기에 코드를 작성해주세요.
