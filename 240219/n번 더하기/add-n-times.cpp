a3dd6e789c47331238a14ed716afa1073c2b637a
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-19 17:47:12 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240219/n번 더하기/add-n-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int a,n;
+using namespace std;
+
+int main() {
+
+    cin >> a>> n;
+
+    for(int i=0; i<n;i++){
+        a+=n;
+        cout << a << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
