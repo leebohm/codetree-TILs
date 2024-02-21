ad8c450e2a25e717dd7b62c3c95e9b6817ba6fc3
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-21 14:30:42 +0900

    Title: 금 채굴하기, Time: 65ms, Memory: 0MB, Status: Passed - CodeTree

--- 240221/금 채굴하기/gold-mining.cpp
+++ 240221/금 채굴하기/gold-mining.cpp
@@ -22,13 +22,15 @@ int GetNumOfGold(int row, int col, int k){
     int dx[4] = {1,1,-1,-1}, dy[4] = {-1,1,1,-1};
 
     num_of_gold += grid[row][col];
+    
 
     for(int curr_k = 1; curr_k <= k; curr_k++){
         int curr_x = row - curr_k, curr_y = col;
+        
 
         for(int curr_dir = 0; curr_dir < 4; curr_dir++){
             for(int step = 0; step < curr_k; step++){
-                if(InRange(curr_k,curr_y)) {
+                if(InRange(curr_x,curr_y)) {
                     num_of_gold += grid[curr_x][curr_y];
                 }
                 curr_x = curr_x + dx[curr_dir];
@@ -45,21 +47,27 @@ int main() {
 
     cin >> n >> m;
 
-    for(int row = 0; row < n; row++)
-        for(int col = 0; col < n; col++)
+
+    for(int row = 0; row < n; row++){
+        for(int col = 0; col < n; col++){
             cin >> grid[row][col];
+        }
+    }
+
 
     for(int row = 0; row <n; row++){
         for(int col = 0; col < n;col++){
             for(int k=0; k<=2*(n-1) ; k++){
                 int num_of_gold = GetNumOfGold(row, col, k);
 
-                if(num_of_gold * m >= GetArea(k))
+                if(num_of_gold * m >= GetArea(k)){
                     max_gold = max(max_gold, num_of_gold);
+                }
             }
         }
     }
 
     cout << max_gold;
     
+    return 0;
 }
