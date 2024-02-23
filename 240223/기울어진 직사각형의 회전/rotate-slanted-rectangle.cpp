965fb3ef0aeec63ec5447a6fc725248a06f392ab
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-23 19:58:22 +0900

    Title: 기울어진 직사각형의 회전, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240223/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
+++ 240223/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
@@ -1,107 +1,65 @@
 #include <iostream>
+#include <vector>
+
+#define MAX_N 100
+#define DIR_NUM 4
+#define CCW 0 
+#define CW 1
 
 using namespace std;
 
-int n,m,r,c,m1,m2,m3,m4,dir;
-int arr[100][100];
+int n;
+int grid[MAX_N][MAX_N];
+int temp[MAX_N][MAX_N];
 
-void Output(){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++)
-            cout << arr[i][j] << " ";
-        
-        cout << endl;
-    }
-}
+void Shift(int x, int y, int k, int l, int move_dir){
+    vector<int> dx, dy, move_nums;
 
-void Move_clockwise(int x, int y){
-    int temp1 = arr[x][y];
-    int temp2 = arr[x-m1][y-m1];
-    int temp3 = arr[x-m1-m2][y-m1+m2];
-    int temp4 = arr[x-m4][y+m4];
-
-    int i = x-m1; int j = y-m1;
-    for(int k = 0; k <m1; k++){
-        arr[i][j] = arr[i+1][j+1];
-        i++; j++;
-    }
-    
-    i = x-m1-m2; j= y-m1+m2;
-    for(int k=0; k < m2; k++){
-        arr[i][j] = arr[i+1][j-1];
-        i++;j--;
-    }
-    arr[x-m1-1][y-m1+1] = temp2;
-
-
-    i = x-m1-m2+m3; j = y-m1+m2+m3;
-    for(int k=0; k<m3;k++){
-        arr[i][j] = arr[i-1][j-1];
-        i--; j--;
-    }
-    arr[x-m1-m2+1][y-m1+m2+1] = temp3; 
-
-    i = x; j = y;
-    for(int k=0; k< m4; k++){
-        arr[i][j] = arr[i-1][j+1];
-        i--;j++;
-    }
-    arr[x-m4+1][y+m4-1] = temp4;
-
-}
-
-void Move_counterclockwise(int x, int y){
-    int temp1 = arr[x][y];
-    int temp2 = arr[x-m1][y+m1];
-    int temp3 = arr[x-m1-m2][y+m1-m2];
-    int temp4 = arr[x-m4][y-m4];
-
-    int i=x-m1; int j= y+m1;
-    for(int k=0; k< m1; k++){
-        arr[i][j] = arr[i+1][j-1];
-        i++;j--;
+    if(move_dir=CCW){
+        dx = {-1,-1,1,1};
+        dy = {1,-1,-1,1};
+        move_nums = {k,l,k,l};
     }
 
-    i = x-m1-m2; j = y+m1-m2;
-    for(int k=0; k<m2; k++){
-        arr[i][j] = arr[i+1][j+1];
-        i++;j++;
+    else {
+        dx = {-1,-1,1,1};
+        dy = {-1,1,1,-1};
+        move_nums = {l, k, l,k};
     }
-    arr[x-m1-1][y+m1-1] = temp2;
-
-    i = x-m4; j = y-m4;
-    for(int k=0; k<m3; k++){
-        arr[i][j] = arr[i-1][j+1];
-        i--; j++;
-    }
-    arr[x-m1-m2+1][y+m1-m2-1] = temp3;
-
-    i=x; j=y;
-    for(int k= 0;k < m4; k++) {
-        arr[i][j] = arr[i-1][j-1];
-        i--;j--;
-    }
-    arr[x-m4+1][y-m4+1] = temp4;
-
-}
-
-
-int main() {
-
-    cin >> n;
 
     for(int i=0; i<n; i++)
         for(int j=0; j<n; j++)
-            cin >> arr[i][j];
+            temp[i][j] = grid[i][j];
     
-    cin >> r >> c >> m1 >> m2 >> m3 >> m4 >> dir;
+    for(int d =0; d<DIR_NUM; d++)
+        for(int q=0; q<move_nums[d]; q++){
+            int nx = x+dx[d] , ny = y+dy[d];
+            temp[nx][ny] = grid[x][y];
+            x = nx; y = ny;
+        }
+    
+    for(int i=0; i< n; i++)
+        for(int j=0; j<n; j++)
+            grid[i][j] = temp[i][j];
+}
 
-    if(dir == 0)
-        Move_counterclockwise(r-1,c-1);
-    else
-        Move_clockwise(r-1,c-1);
+int main(){
+    cin >> n;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin >> grid[i][j];
     
-    Output(); 
-    // 여기에 코드를 작성해주세요.
+    int x, y, m1, m2, m3, m4, d;
+
+    cin >> x >> y >> m1 >> m2 >> m3 >> m4 >> d;
+
+    Shift(x-1, y-1, m1, m2, d);
+
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n; j++)
+            cout << grid[i][j] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
