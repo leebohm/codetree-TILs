449823af52c8c89522c28787708b69505ad9f4b6
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-23 20:26:50 +0900

    Title: 최단 Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+string str;
+int n;
+
+void Shift(){
+    char temp = str[n-1];
+    for(int i=n-2; i>=0; i--){
+        str[i+1] = str[i];
+    }
+    str[0] = temp;
+}
+
+int RunLengthEncoding(){
+    char temp = str[0];
+    int cnt = 1; 
+    for(int i=1; i<n; i++){
+        if(str[i] == temp) continue;
+        else{
+            cnt ++;
+            temp = str[i];
+        }
+    }
+    return cnt*2;
+}
+
+int main() {
+    cin >> str;
+
+    n = str.length();
+    int ans = INT_MAX;
+    for(int i=0; i<n-1; i++){
+        Shift();
+        ans = min(ans, RunLengthEncoding());
+    }
+    cout << ans;
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
