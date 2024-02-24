26408b481b959dd91b63b6e7d8f91941c47a91b8
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-24 22:39:57 +0900

    Title: 십자 모양 폭발, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+int r,c;
+int arr[200][200];
+
+bool InRange( int x, int y){
+    return x>=0 && x < n && y >=0 && y < n;
+}
+
+void Bomb (int x, int y){
+    int step = arr[x][y];
+    //cout << "step : " << step << endl;
+    int dx[4] = {0,-1,0,1};
+    int dy[4] = {1,0,-1,0};
+    for(int i=0; i<4; i++){
+        for(int j=0; j<step; j++){
+            int nx = x+dx[i]*(j);
+            int ny = y+dy[i]*(j);
+            if(InRange(nx,ny)){
+                //cout << nx << " " << ny << endl;
+                arr[nx][ny] = 0;
+            }
+        }
+    }
+}
+int Check(int col){
+    int min_row = n;
+    for(int row = 0; row <n; row++)
+        if(arr[row][col] == 0)
+            min_row = min(min_row,row);
+    return min_row;
+}
+
+void Fall(){
+
+    for(int col=0; col < n; col++){
+        if(Check(col)!= n){
+            int row = Check(col);
+            for(int i=row; i>=1; i--){
+                arr[i][col] = arr[i-1][col];
+                arr[i-1][col] = 0;
+            }
+        }
+    }
+    
+}
+
+int main() {
+
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin >> arr[i][j];
+        
+    
+    cin >> r>> c; r--; c--;
+
+    Bomb(r,c);
+    Fall();
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n;j++){
+            cout  << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+        
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
