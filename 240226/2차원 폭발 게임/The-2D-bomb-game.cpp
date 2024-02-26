1e1f53bdbd8e7c88c80e9968309bd37e994e0bc4
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-26 21:17:29 +0900

    Title: 2차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240226/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 240226/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -81,17 +81,16 @@ int main() {
         for(int j=0; j<n; j++)
             cin >> arr[i][j];
 
-    if(n!=1){
-        for(int i=0; i<k; i++){
+
+    for(int i=0; i<k; i++){
         Bomb();
         Fall();
-        Rotate();
+        if(n!=1)
+            Rotate();
         Fall();
-    }
     }
     
     int ans =0;
-    if(n==0) ans =1;
     
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
