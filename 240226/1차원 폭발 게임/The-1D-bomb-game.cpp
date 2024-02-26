b9277321c8bf05c1b7a8007f7ecb9169fadb5b41
leebohm <102368451+leebohm@users.noreply.github.com>
2024-02-26 16:15:15 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240226/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240226/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -1,90 +1,71 @@
 #include <iostream>
+
 using namespace std;
-
-
 int n,m;
 int arr[100];
-int idx[100];
+int temp[100];
+int end_of_arr, end_of_temp;
 
 
-pair<int,int> Check() {
+int GetEndidx(int curr_idx, int num){
+    int idx = curr_idx;
+    for(int i=curr_idx+1; i<end_of_arr; i++){
+        if(arr[i] == num) idx ++;
+        else break;
+    }
+    return idx--;
 
-    int temp = arr[0];
-    int cnt = 1;
-    int idx =0;
-    int max_cnt=0;
-    for(int i=1; i<n; i++){
-        if(arr[i] == temp){
-            cnt++;
-            max_cnt = max(cnt, max_cnt);
-            if(max_cnt == cnt)
-                idx = i;
-            //cout<< "if : "<<i <<" "<<temp << " " << cnt<< endl;
+}
+
+void FillZero(int sidx, int eidx){
+    for(int i=sidx; i<= eidx;i++)
+        arr[i] = 0;
+}
+
+void MakeTemp (){
+    end_of_temp = 0;
+    for(int i=0; i<end_of_arr; i++){
+        if(arr[i] != 0){
+            temp[end_of_temp++] = arr[i];
         }
-        else{
-            //cout<< "else :" << i<<" " <<temp << endl;
-            temp = arr[i];
-            cnt=1;
-        }
     }
-
-    if(max_cnt >= m)
-        return make_pair(idx,max_cnt);
-    else
-        return make_pair(0,0);
+    end_of_arr = end_of_temp;
 }
 
-
-void MakeZero(int idx, int cnt){
-    for(int i=0; i<n; i++){
-        if(i<=idx && i>=(idx-cnt+1)) arr[i] = 0;
-    }
+void Copy(){
+    for(int i=0; i<end_of_arr; i++)
+        arr[i]= temp[i];
 }
 
-void Fall(){
-    int next_idx = 0;
-    for(int i=0; i < n; i++){
-        if(arr[i] !=0)
-            idx[next_idx++] = arr[i];
-    }
-    for(int i=0; i<next_idx; i++){
-        arr[i] = idx[i];
-    }
-    n = next_idx;
-}
+int main(){
 
-int main() {
-    
-    cin >> n >> m;
+    cin>> n >> m;
 
     for(int i=0; i<n; i++)
         cin >> arr[i];
-    
-    pair<int,int> res = Check();
 
-    int p = res.first;
-    int q = res.second;
+    bool didexplode = false;
 
-    MakeZero(p,q);
-    
+    do{
+        didexplode = false;
 
-    while(p!=0 && q!=0){
-        Fall();
-        res = Check();
-        p = res.first;
-        q = res.second;
-        MakeZero(p,q);
-    }
+        for(int i=0; i<n; i++){
 
+            if(arr[i] == 0) continue;
+            int idx = GetEndidx(i,arr[i]);
 
-    if(n==0)
-        cout << 0;
-    else{
-        cout << n << endl;
-        for(int i=0; i<n; i++)
-                cout << arr[i] << " " ;
-            // 여기에 코드를 작성해주세요.
-    }
-    
+            if(idx - i +1 >= m){
+                didexplode = true;
+                FillZero(i,idx);
+            }   
+
+        }
+        MakeTemp();
+        Copy();
+    }while(didexplode);
+
+    cout << end_of_arr << endl;
+    for(int i=0; i< end_of_arr; i++)
+        cout << arr[i] << endl;
     return 0;
 }
