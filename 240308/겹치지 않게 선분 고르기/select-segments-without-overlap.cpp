0a1c991370e0ccaff3f8f784c582629cb9daaf2d
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-08 14:17:44 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240308/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 240308/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,78 +1,65 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <tuple>
+
+#define MAX_N 15
 
 using namespace std;
 
 int n;
-int k;
-vector<pair<int,int>> line; // 입력되는 선분에 대한 정보
-vector<int> vec; // 어떤 선분을 선택할 것인지에 대한 정보
-bool flag = false;
-int ans = 0; 
+pair<int,int> segments[MAX_N];
 
-bool Overlab(int x1, int y1, int x2, int y2){
-    return (x1 <= y2 )||(x2 <= y1); // true 면 겹치는 것 
+int ans;
+vector<pair<int,int>> selected_segs;
+
+bool Overlapped(pair<int,int> seg1, pair<int,int> seg2){
+    int ax1, ax2;
+    tie(ax1, ax2) = seg1;
+
+    int bx1, bx2;
+    tie(bx1, bx2) = seg2;
+
+    if((ax2 < bx1)|| (bx2 < ax1)) return false;
+    else return true;
 }
 
 bool Possible(){
-    if((int) vec.size() > 1){
-        int x1,y1;
-        int key = vec[0];
-        tie(x1,y1) = line[key -1];
-        int x2,y2;
-        for(int i=1; i<(int) vec.size(); i++){
-            tie(x2,y2) = line[(int) vec[i] -1];
-            if(Overlab(x1,y1,x2,y2) == false){ // 안겹치면 
-                continue;
-            }
-            else return false; // 겹치면 false 반환
-            x1 = x2;
-            y1 = y2;
-        }
-    }
-    else 
-        return true;
-
+    for(int i=0; i<(int) selected_segs.size(); i++)
+        for(int j=i+1; j<(int) selected_segs.size(); j++)
+            if(Overlapped(selected_segs[i], selected_segs[j]))
+                return false;
+    
     return true;
 }
 
-void Choose(int idx){
-    if(idx == k){
-        if(Possible()){ // 안겹치면? 
-            ans = max(ans, (int) vec.size());
-        }
+void FindMaxSegments(int cnt) {
+
+    if(cnt == n){
+        if(Possible())
+            ans = max(ans, (int) selected_segs.size());
         return;
     }
-    for(int i=1;i<=3; i++){
-        vec.push_back(i);
-        Choose(idx+1);
-        vec.pop_back();
-    }
+
+    selected_segs.push_back(segments[cnt]);
+    FindMaxSegments(cnt+1);
+    selected_segs.pop_back();
+
+    //FindMaxSegments(cnt+1);
 }
 
 
-int main() {
-
-    int a,b;
-
+int main(){
     cin >> n;
-    
 
     for(int i=0; i<n; i++){
-        cin >> a >> b;
-        line.push_back(make_pair(a,b));
+        int x1, x2;
+        cin >> x1 >> x2;
+        segments[i] = make_pair(x1,x2);
     }
-    for( k=1; k<=n; k++){ // k 가 의미하는 것은 선분 중에 몇개의 선분을 뽑을 것인지
-        vec.clear();
-        Choose(0);
-    }
+    FindMaxSegments(0);
 
     cout << ans;
-    // 여기에 코드를 작성해주세요.
+
     return 0;
 }
-
-// 겹치치 않게 뽑을 수 있는 최대 선분의 수.. 그러면.. 음 ... 
-//  내 생각에는 .. 먼저 일단 예를 들면 선분 3개 중에 3개 고르거나 2개 골라서 겹치는지 아닌지 판단하고
-// 그 다음에 맥스 값을 계산하면 되는 것 같은데..
