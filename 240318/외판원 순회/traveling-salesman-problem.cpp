5dfafda25dabbb6e0923aeb40830f533d2c2d0cf
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-18 23:56:14 +0900

    Title: 외판원 순회, Time: 418ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/외판원 순회/traveling-salesman-problem.cpp
+++ 240318/외판원 순회/traveling-salesman-problem.cpp
@@ -11,25 +11,42 @@ int ans = INT_MAX;
 vector<int> vec;
 bool visited[MAX_N];
 
+void Print(){
+    cout << "Print : " ;
+    for(int i=0; i<(int) vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
 int Calc(){
+    //Print();
     int sum1 = 0;
-    sum1 += arr[0][vec[0]];
-    for(int i=0; i<(int) vec.size()-1; i++)
-        sum1 += arr[vec[i]][vec[i+1]];
-    sum1 += arr[vec[(int) vec.size()-1]][0];
+    for(int i=0; i<(int) vec.size()-1; i++){
+        if(arr[vec[i]][vec[i+1]] == 0)
+            sum1 += 100000;
+        else{
+            sum1 += arr[vec[i]][vec[i+1]];
+            //cout << arr[vec[i]][vec[i+1]] << " ";
+        }
+    }
+    if(arr[vec[(int) vec.size()-1]][vec[0]] == 0)
+        sum1 += 1000000;
+    else
+        sum1 += arr[vec[(int) vec.size()-1]][vec[0]];
+    //cout << arr[vec[(int) vec.size()-1]][vec[0]] <<" " << sum1 << endl;
+    //cout << endl;
     return sum1;
-
 }
 
 void Choose(int cnt){
-    if(cnt == n-1){
+    if(cnt == n){
         ans = min(ans, Calc());
         return;
     }
 
-    for(int i=1; i<n; i++){
+    for(int i=0; i<n; i++){
         if(visited[i])
             continue;
+
         visited[i] = true;
         vec.push_back(i);
         
