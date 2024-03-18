3f97c664201bb545c287fdefda8be3abe6412235
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-18 23:39:49 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+
+#define MAX_N 10
+using namespace std;
+
+int n;
+int arr[MAX_N][MAX_N];
+int ans = INT_MAX;
+vector<int> vec;
+bool visited[MAX_N];
+
+int Calc(){
+    int sum1 = 0;
+    sum1 += arr[0][vec[0]];
+    for(int i=0; i<(int) vec.size()-1; i++)
+        sum1 += arr[vec[i]][vec[i+1]];
+    sum1 += arr[vec[(int) vec.size()-1]][0];
+    return sum1;
+
+}
+
+void Choose(int cnt){
+    if(cnt == n-1){
+        ans = min(ans, Calc());
+        return;
+    }
+
+    for(int i=1; i<n; i++){
+        if(visited[i])
+            continue;
+        visited[i] = true;
+        vec.push_back(i);
+        
+        Choose(cnt+1);
+        
+        vec.pop_back();
+        visited[i] = false;
+    }
+}
+
+int main() {
+
+    cin >> n;
+    
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin >> arr[i][j];
+
+    Choose(0);
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
