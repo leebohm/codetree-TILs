399d880c26ea732c556ca10f539897a0e4a2de02
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-18 22:28:12 +0900

    Title: 크기가 n인 순열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/크기가 n인 순열/n-permutation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_N 8
+
+using namespace std;
+vector<int> vec;
+int n;
+bool visited[MAX_N];
+
+void Print(){
+    for(int i=0; i<(int) vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
+void Choose(int cnt){
+    if(cnt == n+1){
+        Print();
+        return;
+    }
+
+    for(int i=1; i<=n; i++){
+        if(visited[i])
+            continue;
+
+        visited[i] = true; 
+        vec.push_back(i);
+        Choose(cnt+1);
+        vec.pop_back();
+        visited[i] = false;
+    }   
+}
+
+int main() {
+    cin >> n;
+
+    Choose(1);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
