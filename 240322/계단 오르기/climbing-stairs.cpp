f1050f75270bae10303ead4f4b5c4581ae56f535
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-22 11:21:46 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/계단 오르기/climbing-stairs.cpp
+++ 240322/계단 오르기/climbing-stairs.cpp
@@ -8,16 +8,17 @@ int dp[1001];
 int main() {
 
     cin >> n;
-    
+
     dp[1] = 0;
     dp[2] = 1;
     dp[3] = 1;
     
+
     for(int i=4; i <= n; i++){
-        dp[i] = dp[i-2] + dp[i-3];
+        dp[i] = (dp[i-2] + dp[i-3]) % 10007;
     }
 
-    cout << dp[n] % 10007;
+    cout << dp[n];
 
     // 여기에 코드를 작성해주세요.
     return 0;
