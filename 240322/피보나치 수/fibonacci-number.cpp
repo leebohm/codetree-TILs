346eabd5264a2016f507e582912746efc9091699
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-22 11:13:45 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[46];
+int n;
+
+int main() {
+
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for(int i = 3; i<=n; i++){
+        dp[i] = dp[i-1]+dp[i-1];
+    }
+
+    cout << dp[n];
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
