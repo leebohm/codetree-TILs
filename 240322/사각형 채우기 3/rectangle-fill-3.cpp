0acd99b1d6c94ebe0e06c1ee94787ffa6a59050e
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-22 11:50:14 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240322/사각형 채우기 3/rectangle-fill-3.cpp
@@ -14,7 +14,7 @@ int main(){
     dp[0] = 1;
     dp[1] = 2;
 
-    for(int i=2; i<=2; i++){
+    for(int i=2; i<=n; i++){
         dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % MOD;
         for(int j=i-3; j>=0; j--)
             dp[i] = (dp[i] + dp[j]*2) % MOD;
