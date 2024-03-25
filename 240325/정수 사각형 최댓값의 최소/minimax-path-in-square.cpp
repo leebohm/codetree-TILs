4416998468a773345d9f0306c9161dd423ca3e29
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-25 22:29:20 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240325/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
+++ 240325/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -20,7 +20,7 @@ void Initialize(){
         dp[i][0] = max(dp[i-1][0],arr[i][0]);
     
     for(int j=1; j<n; j++)
-        dp[0][j] = max(dp[0][j-1],arr[0][j-1]);
+        dp[0][j] = max(dp[0][j-1],arr[0][j]);
 }
 
 int main() {
