02d9cfae56a23ca7035010a3ae57a3cc2a3bd08c
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-26 15:06:06 +0900

    Title: 최대 감소 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
+++ 240326/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
-#include <climits>
+#include <algorithm>
 
 #define MAX_N 1000
+
 using namespace std;
 
 int n;
-int a[MAX_N+1];
-int dp[MAX_N+1];
+int arr[MAX_N];
 
-void Initialize(){
-    //for(int i=0; i<=n; i++)
-     //   dp[i] = INT_MIN;
+int dp[MAX_N];
 
-    dp[0] = 0;
-    a[0] = 0;
-}
-
-int main() {
+int main(){
 
     cin >> n;
 
-    for(int i=1; i<=n; i++)
-        cin >> a[i];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    
+    for(int i=0; i<n; i++){
+        dp[i] = 1;
 
-    Initialize();
-
-    for(int i=1; i<=n; i++)
-        for(int j=0; j<i; j++)
-            if(a[i] < a[j])
+        for(int j=0; j<i; j++){
+            if(arr[j] > arr[i])
                 dp[i] = max(dp[i], dp[j]+1);
+        }
+    }
 
     int ans = 0;
 
-    for(int i=1; i<=n; i++){
+    for(int i=0; i<n; i++)
         ans = max(ans, dp[i]);
-        //cout << dp[i] << " ";
-    }
 
-    
-    cout << ans+1;
-
-    // 여기에 코드를 작성해주세요.
+    cout << ans;
     return 0;
 }
