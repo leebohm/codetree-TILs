c9ebabb1d138b5594289e5a3c478f63ec99174b0
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-31 16:54:32 +0900

    Title: 트리 정점 거리, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240331/트리 정점 거리/node-distance.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n,m;
+vector<pair<int,int>> edges[MAX_N+1];
+bool visited[MAX_N]+1;
+int dist[MAX_N+1][MAX_N+1];
+
+void DFS(int st, int x){
+    for(int i =0; i<(int) edges[x].size(); i++){
+        int y,d;
+        tie(y,d) = edges[x][i];
+
+        if(visited[y]) continue;
+
+        visited[y] = true;
+        dist[st][y] = dist[st][x] +d;
+        DFS(st,y);
+    }
+}
+
+int main(){
+
+    cin >> n >> m;
+    for(int i=1; i<=n-1; i++){
+        int x,y,d;
+        cin >> x >> y >> d;
+
+        edges[x].push_back({y,d});
+        edges[y].push_back({x,d});
+
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++)
+            visited[j] = false;
+
+        visited[i] = true;
+        DFS(i,i);
+    }
+
+
+    for(int i=1; i<=m; i++){
+        int x,y;
+        cin >> x >> y;
+        cout << dist[x][y] << endl;
+    }
+    return 0;
+}
