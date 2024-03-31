00f20f7e0b23c61c774662295e3c4bbda93a1928
leebohm <102368451+leebohm@users.noreply.github.com>
2024-03-31 12:26:03 +0900

    Title: 트리 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240331/트리 판별/tree-identification.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_N 10001
+
+using namespace std;
+
+vector<int> edges[MAX_N];
+int m;
+int comein[MAX_N];
+int ans = 0;
+bool visited[MAX_N];
+
+void DFS(int x){
+    
+    for(int i=0; i<(int)edges[x].size(); i++){
+        int y = edges[x][i];
+        
+        if(!visited[y]){
+            ans++;
+            DFS(y);
+        }
+
+    }
+}
+
+int main() {
+
+    int x, y;
+    cin >> m;
+    
+    int max_node = 0;
+
+    for(int i=0; i<m; i++){
+        cin >> x >> y;
+        max_node = max(max_node,max(x,y));
+        comein[y]++;
+        edges[x].push_back(y);
+    }
+
+    int root = 0;
+
+    for(int i=1; i<= max_node ;i++ ){
+        if((int) edges[i].size() > 0 && comein[i] == 0)
+            root = i;
+    }
+
+    visited[root] = true;
+    DFS(root);
+
+    if(ans == m)
+        cout << 1;
+    else
+        cout << " " <<0;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
