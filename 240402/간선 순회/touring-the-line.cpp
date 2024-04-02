2c57dc65a9ded15a31075dd67ddb3a6d7c23de45
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-02 11:44:42 +0900

    Title: 간선 순회, Time: 70ms, Memory: 4MB, Status: Passed - CodeTree

--- 240402/간선 순회/touring-the-line.cpp
+++ 240402/간선 순회/touring-the-line.cpp
@@ -2,81 +2,75 @@
 #include <vector>
 #include <tuple>
 
-#define MAX_N 50001
+#define MAX_N 50000
 
 using namespace std;
 
-int n,d;
-vector<pair<int,int>> edge[MAX_N];
-bool used[MAX_N];
-int dist[MAX_N];
-bool visited[MAX_N];
+// 변수 선언:
+int n, d;
+vector<pair<int, int> > edge[MAX_N + 1];
+bool visited[MAX_N + 1];
+int dist_node[MAX_N + 1];
+int dist[MAX_N + 1];
+pair<int, int> max_dist;
+int last_node;
 
-void DFS(int x){
-    for(int i=0; i<edge[x].size(); i++){
-        int y,tmp;
-        tie(y,tmp) = edge[x][i];
-        if(!visited[y]){
-            dist[y] = dist[x] + tmp;
-            visited[y] = true;
-            DFS(y);
+// 모든 노드의 정점을 탐색하는 DFS를 진행합니다.
+void DFS(int x) {
+    visited[x] = true;
+
+    for(int i = 0; i < edge[x].size(); i++) {
+        int y, dis;
+        tie(y, dis) = edge[x][i];
+
+        // 이미 방문한 정점이면 스킵합니다.
+        if(visited[y]) continue;
+
+        dist_node[y] = dist_node[x] + 1;
+        dist[y] = dist[x] + dis;
+
+        pair<int, int> cur_dist = {dist_node[y], -dist[y]};
+
+        // 현재 정점을 기준으로 가장 먼 노드를 찾습니다.
+        // 지나는 간선의 수가 최대가 되도록 하며,
+        // 동일한 경우 거리의 합이 최소가 되도록 합니다.
+        if(cur_dist > max_dist) {
+            max_dist = cur_dist;
+            last_node = y;
         }
-    }
-}
 
-void Initialize(){
-    for(int i= 0; i< MAX_N; i++){
-        dist[i] = 0;
-        visited[i] = false;
+        DFS(y);
     }
-}
-
-pair<int,int> FindMax(){
-    int d_  = 0;
-    int idx = 0;
-    for(int i=0; i<MAX_N; i++){
-        if(used[i]){
-            if(d_ < dist[i]){
-                d_ = dist[i];
-                idx = i;
-            }
-        }
-    }
-    Initialize();
-    return make_pair(idx, d_);
 }
 
 int main() {
-
+    // 입력:
     cin >> n >> d;
 
-    int root = 0;
-    for(int i=0; i<n-1; i++){
-        int x,y,dist;
-        cin >> x >> y >> dist;
+    // n - 1개의 간선 정보를 입력받습니다.
+    for(int i = 1; i <= n - 1; i++) {
+        int x, y, dis;
 
-        edge[x].push_back(make_pair(y,dist));
-        edge[y].push_back(make_pair(x,dist));
-        used[x] = used[y] = true;
-        root = x;
-        
+        cin >> x >> y >> dis;
+
+        edge[x].push_back({y, dis});
+        edge[y].push_back({x, dis});
     }
 
-    visited[root] = true;
-    DFS(root);
+    // DFS를 통해 가장 먼 노드를 찾습니다.
+    DFS(1);
 
-    int f_vertex, f_dist;
-    tie(f_vertex,f_dist) = FindMax();
+    // 가장 먼 노드에서 시작해 다시 한번 DFS를 돌려 트리의 가장 긴 거리를 찾습니다.
+    for(int i = 1; i <= n; i++) {
+        visited[i] = false;
+        dist_node[i] = 0;
+        dist[i] = 0;
+    }
 
-    visited[f_vertex] = true;
-    DFS(f_vertex);
+    DFS(last_node);
 
-    tie(f_vertex,f_dist) = FindMax();
-
-    if(f_dist/d)
-        cout << f_dist / d + 1;
-    else
-        cout << f_dist / d;
-    // 여기에 코드를 작성해주세요.
+    // 꼭 필요한 날짜의 수를 출력합니다.
+    //cout <<  max_dist.first << " " << max_dist.second << endl;
+    cout << 1 + (-max_dist.second ) / d;
     return 0;
 }
