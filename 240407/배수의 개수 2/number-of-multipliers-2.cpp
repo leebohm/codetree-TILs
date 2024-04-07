0c2921e703d8f4944d1c185545736f38cf10063c
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-07 23:47:09 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240407/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    
+    int cnt =0;
+
+    for(int i=0; i<10; i++){
+        cin >> n;
+        if(n % 2 == 1)
+            cnt++;
+    }
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
