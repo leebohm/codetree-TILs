ee192e769e5125de42a4dac354bccf57eef20c18
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-07 16:55:57 +0900

    Title: 최대로 연속한 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240407/최대로 연속한 숫자/maximum-consecutive-number.cpp
+++ 240407/최대로 연속한 숫자/maximum-consecutive-number.cpp
@@ -9,26 +9,21 @@ set<int> s_num;
 set<tuple<int,int,int> > s_len;
 
 int main(){
-
     cin >> n >> m;
-    
     s_num.insert(-1);
     s_num.insert(n+1);
-
-    s_len.insert(make_tuple(-(n+1),-1,n+1));
+    s_len.insert(make_tuple((-n+1), -1, n+1));
 
     for(int i=0; i<m; i++){
-
         int y;
         cin >> y;
-
+        
         s_num.insert(y);
 
         set<int>::iterator it;
         it = s_num.find(y);
         it++;
         int z = *it;
-
         it = s_num.find(y);
         it--;
         int x = *it;
@@ -36,7 +31,6 @@ int main(){
         s_len.erase(make_tuple(
             -(z-x-1),x,z
         ));
-
         s_len.insert(make_tuple(
             -(y-x-1),x,y
         ));
@@ -46,8 +40,7 @@ int main(){
 
         int best_length;
         tie(best_length, ignore, ignore) = *s_len.begin();
-        cout << -best_length << endl;
+        cout << - best_length << endl;
     }
-
     return 0;
 }
