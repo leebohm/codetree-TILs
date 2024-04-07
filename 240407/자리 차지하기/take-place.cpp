421d4dfd1d2840b88394816f9a58f5d772bbe9aa
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-07 14:46:53 +0900

    Title: 자리 차지하기, Time: 29ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240407/자리 차지하기/take-place.cpp
+++ 240407/자리 차지하기/take-place.cpp
@@ -5,14 +5,30 @@ using namespace std;
 
 set<int> s;
 int n,m;
+int arr[100000];
 
 int main() {
 
     cin >> n >>  m;
 
     for(int i=0; i<n; i++){
-        int tmp;
-        cin >> tmp;
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++){
+        int tmp = arr[i];
+        while(tmp--){
+            if(s.find(tmp) == s.end()){
+                s.insert(tmp);
+                break;
+            }
+                
+        }
+
+        if(tmp== 0 ){
+            break;
+        }
+
         s.insert(tmp);
     }
 
