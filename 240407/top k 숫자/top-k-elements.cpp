4ceadc8f76dd2311a8d348f6c11c8e989b91b7fb
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-07 11:47:43 +0900

    Title: top k 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240407/top k 숫자/top-k-elements.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int n,k;
+set<int> s;
+
+int main() {
+    
+    cin >> n >> k;
+
+    for(int i=0; i<n; i++){
+        int tmp;
+        cin >> tmp;
+        s.insert(tmp);
+    }
+
+    set<int>::iterator it = s.lower_bound(1000000000);
+
+    for(int i=0; i<k; i++){
+        cout << *it << " " ;
+        it --;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
