27032e4cbd2f9bef7c9c3aec2eb8d196ef02d2a2
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-07 15:21:09 +0900

    Title: 자리 차지하기, Time: 1053ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240407/자리 차지하기/take-place.cpp
+++ 240407/자리 차지하기/take-place.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <set>
 
-#define MAX_N 100000
-
 using namespace std;
 
+set<int> s;
 int n,m;
-int arr[MAX_N];
-
-set<int> seats;
-
-int main(){
-    cin >>  n >> m;
-    
-    for(int i=0; i<n; i++)
-        cin >> arr[i];
-    
-    for(int i=1; i<=m; i++)
-        seats.insert(i);
-    
-    int ans = 0;
-
+int arr[100000];
+int main() {
+    cin >> n >> m;
+   
     for(int i=0; i<n; i++){
-        set<int>::iterator idx = seats.upper_bound(arr[i]);
-    
-        if(idx != seats.begin()){
-            idx--;
-            seats.erase(*idx);
-
-            ans++;
-        }
-        else break;
+        cin>>arr[i];
     }
 
+    for(int i=0; i<n; i++){
+        int tmp = arr[i];
+        while(tmp != 0){
+            if(s.find(tmp)==s.end()){
+                s.insert(tmp);
+                break;
+                }
+            tmp--;
+            
+        }
+	    if(tmp==0)
+		    break;
+    }
+	int ans = (int) s.size();
     cout << ans;
     return 0;
-
 }
