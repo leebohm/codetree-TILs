7e270c34b53366a07e4c3a549cd1ee27601f694d
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-08 15:32:13 +0900

    Title: 마지막으로 남은 숫자, Time: 1074ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240408/마지막으로 남은 숫자/last-remaining-number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n;
+priority_queue<int> pq;
+int x,y;
+
+int main() {
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> x;
+        pq.push(x);
+    }
+
+    while((int) pq.size()!= 1){
+        x = pq.top(); pq.pop();
+        y = pq.top(); pq.pop();
+        if(abs(x-y) != 0){
+            pq.push(abs(x-y));
+        }
+    }
+
+    int ans = pq.top();
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
