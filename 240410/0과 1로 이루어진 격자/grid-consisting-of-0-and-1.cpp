2c9f4aa9e74d6a1a7306c0082cc947df87030f07
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-10 10:46:51 +0900

    Title: 0과 1로 이루어진 격자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240410/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.cpp
+++ 240410/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.cpp
@@ -40,13 +40,13 @@ int main() {
     }
        
     
-    for(int i=1;i<n; i++){
-        for(int j=0; j<n; j++){
-            if(arr[i-1][j] == 1){
+    for(int i=n-1;i>=0; i--){
+        for(int j=n-1; j>=0; j--){
+            if(arr[i][j] == 1 ){
                 Change(i,j);
                 ans++;
             }
-            Output();
+            //Output();
         }
     }
     cout << ans;
