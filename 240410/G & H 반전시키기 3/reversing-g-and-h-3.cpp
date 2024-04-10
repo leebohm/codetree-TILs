6ad44d713b86debc2a9b2a1c07d0aa5e2fa6adc6
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-10 11:05:25 +0900

    Title: G & H 반전시키기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240410/G & H 반전시키기 3/reversing-g-and-h-3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string src;
+string des;
+int cnt;
+int ans;
+void Change(int x, int y){
+    for(int i = x-y+1; i<=x; i++ ){
+        if(src[i] == 'G')
+            src[i] = 'H';
+        else
+            src[i] = 'G';
+    }
+}
+int main() {
+
+    cin >> n >> src >> des;
+    
+    bool flag = true;
+    for(int i=0; i<n; i++){
+        if(cnt == 4){
+            Change(i-1,cnt);
+            ans++;
+            flag = true;
+            cnt = 0;
+        }
+        if(src[i] != des[i] && flag == true){
+            cnt++;
+            flag = false;
+        }
+        else if(src[i]!= des[i] && flag == false){
+            cnt++;
+        }
+        else if(src[i] == des[i]){
+            if(cnt >=1){
+                ans++;
+                Change(i-1,cnt);
+                //cout << "i : " << i <<" , cnt : "<<cnt  <<" " << src << endl;
+            }
+            
+            cnt = 0;
+            flag = true;
+        }
+        //cout << "i : " << i <<" , cnt : "<<cnt  <<" " << src << endl;
+    }
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
