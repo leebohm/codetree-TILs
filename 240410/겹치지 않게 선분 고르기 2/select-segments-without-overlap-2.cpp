a2b774bdf90d68cb003199ead3b93321167c9cdf
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-10 15:52:41 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240410/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <tuple>
+#include <climits>
+
+#define MAX_N 1001
+
+using namespace std;
+
+int n;
+int x,y;
+pair<int,int> xy[MAX_N];
+int dp[MAX_N];
+int max_y = 0;
+
+void Initialize(){
+    
+    for(int i=0; i<max_y; i++)
+        dp[i] = -1;
+
+    tie(x,y) = xy[0];
+    dp[y] = 1;
+}
+
+void Output(){
+    for(int i=0; i<=max_y; i++)  
+        cout << dp[i]  <<" ";
+    cout << endl;
+}
+
+int main() {
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> x >> y;
+        max_y = max(y, max_y);
+        xy[i] = make_pair(x,y);
+    }
+
+    Initialize();
+
+    for(int i=1; i<n; i++){
+        int cx,cy;
+        tie(cx,cy) = xy[i];
+        for(int j=0; j<i; j++){
+            int bx, by;
+            tie(bx, by) = xy[j];
+            if(by < cx){
+                dp[cy] = max(dp[by]+1, dp[cy]);
+            }
+
+        }
+        //Output();
+    }
+
+    int ans = 0;
+    for(int i=0; i<= max_y; i++)
+        ans = max(ans, dp[i]);
+
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
