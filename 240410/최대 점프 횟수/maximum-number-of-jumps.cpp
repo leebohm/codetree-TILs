79ef612a5e775466f6839b460741af7090c1ec07
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-10 15:10:25 +0900

    Title: 최대 점프 횟수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240410/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define MAX_N 1001
+using namespace std;
+
+int n;
+int arr[MAX_N];
+int dp[MAX_N];
+
+void Output(){
+    for(int i=0; i<n; i++)
+        cout << dp[i] << " ";
+    cout << endl;
+}
+int main(){
+    
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+    for(int i=0; i<n; i++){
+        int tmp = arr[i];
+        if(tmp != 0){
+            for(int j=i+1; j<=i+tmp; j++){
+                dp[j] = max(dp[i]+1, dp[j]);
+            }
+        }
+        //Output();       
+    }
+    int ans = 0;
+
+    for(int i=0; i<n; i++)
+        ans = max(ans, dp[i]);
+    
+    cout << ans;
+    
+    return 0;
+}
