6e555ae21cf348fbe10d07e4d5e5f1ea19057da8
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-11 17:20:01 +0900

    Title: 낮은 지점들, Time: 53ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240411/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int n;
+unordered_map<int,int> xy;
+int x, y;
+
+int main() {
+    
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> x >> y;
+        if(xy.find(x) == xy.end())
+            xy[x] = y;
+        else{
+            if(xy[x] < y) // 기존것이 더 작은 상황
+                continue;
+            else
+                xy[x] = y; // 새로 들어온 것이 더 작은 상황.
+        }
+    }
+
+    int ans = 0;
+    for(unordered_map<int,int>::iterator it = xy.begin(); it != xy.end(); it++)
+        ans += it->second;
+    
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
