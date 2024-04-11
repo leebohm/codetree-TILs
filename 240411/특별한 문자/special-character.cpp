ea3e06d4018c58530b343196c13060dd8e02e6d4
leebohm <102368451+leebohm@users.noreply.github.com>
2024-04-11 17:08:00 +0900

    Title: 특별한 문자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240411/특별한 문자/special-character.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+string str;
+unordered_map<char, int> freq;
+
+int main() {
+
+    cin >> str;
+
+    for(int i=0; i<(int)str.size(); i++){
+        if(freq.find(str[i]) == freq.end())
+            freq.insert({str[i],1});
+        else
+            freq[str[i]]++;         
+    }
+    bool flag = false;
+    for(unordered_map<char,int>::iterator it = freq.begin(); it!= freq.end(); it++){
+        if(it->second == 1){
+            cout << it->first;
+            flag = true;
+            break;
+        }
+    }
+
+    if(flag == false)
+        cout << "None";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
