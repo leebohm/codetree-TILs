ec975dad13ddfefc7be7a96722ec5290ce9e022a
leebohm <102368451+leebohm@users.noreply.github.com>
2024-06-27 21:10:08 +0900

    Title: 서로 다른 k개의 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240627/서로 다른 k개의 문자/k-distinct-characters.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+string str;
+int k;
+unordered_map<char,int> m;
+
+int main() {
+
+    cin >> str >> k;
+    int n = (int) str.size();
+
+    str = "#" + str;
+
+    int j =0; 
+    int ans = 0;
+
+    for(int i=1; i<=n; i++){
+        while(j+1 <=n && m.size() <= k){
+            //cout << j+1  << " " << i<< endl;
+            m[str[j+1]]++;
+            j++;
+        }
+        if(m.size() <= k){
+            ans = max(ans, j-i+1);
+            //cout << "m.size() :  " << m.size() << endl;
+        }
+        m[str[i]]--;
+    }
+
+    cout << ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
