60f107e4bfae5581ef7128aaa4d34d111d30cda4
leebohm <102368451+leebohm@users.noreply.github.com>
2024-06-27 22:44:00 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240627/양쪽 모두 존재하는 구간/both-exist-section.cpp
+++ 240627/양쪽 모두 존재하는 구간/both-exist-section.cpp
@@ -1,78 +1,77 @@
 #include <iostream>
-#include <algorithm>
 #include <climits>
 
 #define MAX_N 100001
-#define MAX_M 100001
 
 using namespace std;
 
 int n,m;
 int arr[MAX_N];
-int count_array_in[MAX_M];
-int count_array_out[MAX_M];
+int count_in[MAX_N];
+int count_out[MAX_N];
 
-int distinct_num_in;
-int distinct_num_out;
+int distinct_in;
+int distinct_out;
 
 void Push(int idx){
     int num = arr[idx];
 
-    if(count_array_in[num] == 0)
-        distinct_num_in++;
-    count_array_in[num]++;
+    if(count_in[num] == 0)
+        distinct_in++;
+    count_in[num]++;
 
-    count_array_out[num]--;
-    if(count_array_out[num]== 0)
-        distinct_num_out--;
+    count_out[num]--;
+    if(count_out[num] == 0)
+        distinct_out--;
 }
 
 void Pop(int idx){
     int num = arr[idx];
 
-    count_array_in[num]--;
-    if(count_array_in[num]==0)
-        distinct_num_in--;
-    
-    if(count_array_out[num]==0)
-        distinct_num_out++;
-    count_array_out[num]++;
+    if(count_out[num] == 0)
+        distinct_out++;
+    count_out[num]++;
+
+    count_in[num]--;
+    if(count_in[num] == 0)
+        distinct_in--;
 }
 
 int main(){
-    cin >> n >> m;
+    cin >> n>>m;
 
-    for(int i=1; i<=n; i++)
+    for(int i=1;i<=n; i++)
         cin >> arr[i];
     
     for(int i=1; i<=n; i++){
-        if(count_array_out[arr[i]] == 0)
-            distinct_num_out++;
-        
-        count_array_out[arr[i]]++;
+        if(count_out[arr[i]] == 0)
+            distinct_out++;
+        count_out[arr[i]]++;
     }
 
+    int j =0;
     int ans = INT_MAX;
 
-    int j=0; 
     for(int i=1; i<=n; i++){
-        while(j+1 <=n && distinct_num_in < m){
+        while(j+1 <=n && distinct_in < m){
             Push(j+1);
             j++;
         }
 
-        if(distinct_num_in < m)
+        if(distinct_in < m)
             break;
         
-        if(distinct_num_in == m)
-            ans = min(ans, j-i+1);
+        if(distinct_out == m)
+            ans= min(ans, j-i+1);
         
-        Pop(i);
+        count_in[arr[i]]--;
+        if(count_in[arr[i]] == 0)
+            distinct_in--;
     }
 
     if(ans == INT_MAX)
         ans = -1;
-    
     cout << ans;
+
     return 0;
 }
