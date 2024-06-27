2b38709df18246a5293b2e57d5ed6282ba559db1
leebohm <102368451+leebohm@users.noreply.github.com>
2024-06-27 20:32:50 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240627/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.cpp
+++ 240627/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
+#include <unordered_map>
 #include <string>
 
 using namespace std;
 
 string str;
+int n;
+unordered_map<char,int> count;
 
-int main() {
-
+int main(){
+    
     cin >> str;
-    int n = str.size();
+    n = (int) str.size();
+    str = "#" + str;
 
     int j = 0;
-    int max_num = 0;
-
-    for(int i=0; i<n; i++){
-        while(j+1>i && str[i] != str[j+1])
+    int ans = 0;
+    for(int i=1; i<=n; i++){
+        while(j+1 <=n && count[str[j+1]]!= 1){
+            count[str[j+1]]++;
             j++;
-        if(j >= n)
-            break;
-        
-        max_num = max(max_num, j-i+1);
+        }
+
+        ans = max(ans, j-i+1);
+        count[str[i]]--;
     }
-    if(max_num == 0)
-        max_num = 1;
-    cout << max_num;
-    // 여기에 코드를 작성해주세요.
-    return 0;
+
+    if(ans ==0)
+        ans =1;
+    cout << ans;
+
 }
