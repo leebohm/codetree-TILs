efe16d33c3ae8e74a857acca88b7642ef7b1f6af
leebohm <102368451+leebohm@users.noreply.github.com>
2024-07-02 09:30:18 +0900

    Title: 연결리스트 1, Time: 530ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240702/연결리스트 1/linked-list1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    string data;
+    Node *prev, *next;
+    Node(string data) : data(data), prev(nullptr), next(nullptr){};
+};
+
+void connect(Node *s, Node *e){
+    if(nullptr != s) s-> next = e;
+    if(nullptr != e) e-> prev = s;
+}
+
+void insertNext(Node *target, Node *s){
+    connect(s,target->next);
+    connect(target,s);
+}
+
+void insertPrev(Node *target, Node *s){
+    connect(target->prev, s);
+    connect(s,target);
+}
+
+void printNode(Node *target){
+    string n = "(Null)";
+
+    if(target->prev == nullptr) cout << n << " ";
+    else cout << target->prev->data <<" ";
+
+    cout << target->data << " ";
+
+    if(target->next == nullptr) cout << n <<"\n";
+    else cout << target->next->data <<"\n";
+}
+
+int main(){
+    string sInit;
+    cin >> sInit;
+
+    Node *cur = new Node(sInit);
+
+    int n;
+    cin >> n;
+
+    while(n--){
+        int option;
+        cin >> option;
+
+        if(option == 1){
+            string data;
+            cin >> data;
+            Node *target = new Node(data);
+
+            insertPrev(cur, target);
+        }
+
+        if(option == 2){
+            string data;
+            cin >> data;
+            Node *target = new Node(data);
+
+            insertNext(cur,target);
+        }
+
+        if(option == 3)
+            if(cur->prev != nullptr) cur = cur->prev;
+        
+        if(option == 4)
+            if(cur-> next != nullptr) cur = cur->next;
+
+        printNode(cur);
+    }
+
+    return 0;
+}
