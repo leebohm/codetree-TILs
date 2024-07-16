bb5d58bc9cbc47ec6beccabf7bf6bc4c8f928744
leebohm <102368451+leebohm@users.noreply.github.com>
2024-07-17 00:58:48 +0900

    Title: 연결리스트 1, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/연결리스트 1/linked-list1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string id;
+    Node *prev, *next;
+    Node (string id) : id(id) , prev(nullptr), next(nullptr){}
+};
+
+void insertPrev (Node *s, Node *e){
+    if(s != nullptr) s->prev = e;
+    if(e != nullptr) e->next = s;
+}
+
+void insertNext (Node *s , Node *e){
+    if(s != nullptr) s->next = e;
+    if(e != nullptr) e->prev = s;
+}
+int n;
+
+
+int main() {
+
+    string str;
+
+    cin >> str;
+    
+    Node *cur = new Node(str);
+
+    cin >> n;
+    int opt = 0;
+
+    for(int i=0; i<n; i++){
+        cin >> opt;
+        if(opt == 1){
+            cin >> str;
+            Node *tmp = new Node(str);
+            insertPrev(cur, tmp);
+
+        }
+        if(opt == 2){
+            cin >> str;
+            Node *tmp = new Node(str);
+            insertNext(cur, tmp);
+
+        }
+        if(opt == 3){
+            if(cur->prev != nullptr)
+                cur = cur->prev;
+        }
+        if(opt == 4){
+            if(cur->next != nullptr)
+                cur = cur->next;
+        }
+
+        if(cur->prev == nullptr)
+            cout << "(Null)" << " ";
+        else
+            cout << cur->prev->id << " ";
+
+        if(cur == nullptr)
+            cout << "(Null)" << " ";
+        else
+            cout << cur->id << " ";
+
+        if(cur->next == nullptr)
+            cout << "(Null)" << endl;
+        else
+            cout << cur->next->id << endl;
+
+            
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
