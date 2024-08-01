c2a71847af0e4dbe1d0718c1ab20beae969c84ea
leebohm <102368451+leebohm@users.noreply.github.com>
2024-08-01 09:54:15 +0900

    Title: 코드트리 유치원, Time: 211ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240801/코드트리 유치원/codetree-kindergarden.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX_N 200000
+
+struct Node {
+    int id;
+    Node *prev, *next;
+    Node(int id) : id(id), prev(nullptr), next(nullptr) {}
+};
+
+Node *nodes[MAX_N+2];
+
+int nodeCnt;
+
+void connect(Node *s, Node *e){
+    if(nullptr != s) s->next = e;
+    if(nullptr != e) e->prev = s;
+}
+
+void insertNextRange(Node *s, Node *e, Node *v){
+    Node *nextV = v->next;
+    connect(v,s);
+    connect(e,nextV);
+}
+
+void insertPrevRange(Node *s, Node *e, Node *v){
+    connect(v->prev, s);
+    connect(e,v);
+}
+
+int main(){
+
+    int q;
+    cin >> q;
+
+    nodeCnt = 1;
+    nodes[1] = new Node(1);
+
+    while(q--){
+        int opt;
+        cin >> opt;
+
+        if(opt == 1){
+            int a,b;
+            cin >> a >> b;
+            int init = nodeCnt + 1;
+            nodeCnt += b;
+
+            for(int i=0; i<b; i++){
+                nodes[init+i] = new Node(init + i);
+                if(i!= 0) connect(nodes[init+i-1],nodes[init+i]);
+            }
+
+            insertNextRange(nodes[init], nodes[init+b-1], nodes[a]);
+        }
+
+        if(opt == 2){
+            int a,b;
+            cin >> a >> b;
+            int init = nodeCnt + 1;
+            nodeCnt += b;
+
+            for(int i=0; i<b; i++){
+                nodes[init+i] = new Node(init + i);
+                if(i != 0) connect(nodes[init+i-1], nodes[init+i]);
+            }
+
+            insertPrevRange(nodes[init], nodes[init+b-1], nodes[a]);
+        }
+
+        if(opt == 3){
+            int a;
+            cin >> a;
+            if(nodes[a]->prev == nullptr || nodes[a]->next == nullptr ) cout << -1 << "\n";
+            else cout << nodes[a]->prev->id << " " << nodes[a]->next->id << "\n";
+        }
+    }
+}
