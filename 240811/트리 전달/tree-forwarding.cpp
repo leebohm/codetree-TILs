db3b49153ca118e22809d09805dc72ae8439357e
leebohm <102368451+leebohm@users.noreply.github.com>
2024-08-11 21:00:14 +0900

    Title: 트리 전달, Time: 7ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 240811/트리 전달/tree-forwarding.cpp
+++ 240811/트리 전달/tree-forwarding.cpp
@@ -8,14 +8,18 @@ int n,m; // 노드의 수 : n, 초기 연산의 수 : m
 int parents[MAX_N];
 vector<int> children[MAX_N];
 int score[MAX_N];
+bool visited[MAX_N];
 
-void DFS(int idx, int w){
+void DFS(int idx){
 
     int num = children[idx].size();
-    score[idx] += w;
     for(int i=0; i<num; i++){
         int tmp = children[idx][i];
-        DFS(tmp,w);
+        if(visited[tmp] == false){
+            DFS(tmp);
+            score[tmp] += score[idx];
+        }
+        
     }
 }
 
@@ -31,8 +35,12 @@ int main() {
     for(int i=1; i<=m; i++){
         int idx, w;
         cin >> idx >> w;
-        DFS(idx,w);
+        score[idx] = w;
     }
+
+    visited[1] = true;
+    DFS(1);
+
     for(int i=1; i<=n; i++){
         cout << score[i] << " ";
     }
