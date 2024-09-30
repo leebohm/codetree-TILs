0911f14b9004290f02d41f161989c08ea6d8abbc
leebohm <102368451+leebohm@users.noreply.github.com>
2024-09-30 11:26:43 +0900

    Title: 겹치지 않는 선분, Time: 89ms, Memory: 1MB, Status: Passed - CodeTree

--- 240930/겹치지 않는 선분/line-segments-that-do-not-overlap.cpp
+++ 240930/겹치지 않는 선분/line-segments-that-do-not-overlap.cpp
@@ -1,94 +1,47 @@
 #include <iostream>
-#include <set>
-#include <unordered_map>
+#include <algorithm>
+#include <tuple>
+
+#define MAX_N 100000
 
 using namespace std;
 
-#define MAX_N 100001
-
-int x1[MAX_N];
-int x2[MAX_N];
-set<int> s1;
-set<int> s2;
-set<int> ans_set;
-unordered_map<int,int> x1_2_x2;
-unordered_map<int,int> x2_2_x1;
-
 int n;
 
-bool Check(int a, int b, int c, int d){
-    if(a < c && d < b)
-        return true;
-    return false;
-}
+pair<int,int> segments[MAX_N];
+int L[MAX_N], R[MAX_N];
 
-int main() {
-    
+int main(){
     cin >> n;
 
     for(int i=0; i<n; i++){
-        cin >> x1[i] >> x2[i];
-        x1_2_x2[x1[i]] = x2[i];
-        x2_2_x1[x2[i]] = x1[i]; 
-        s1.insert(x1[i]);
-        s2.insert(x2[i]);
+        int x1, x2;
+        cin >> x1 >> x2;
+        segments[i] = make_pair(x1,x2);
     }
 
-    for(int i=0; i<n; i++){
-        set<int>::iterator it;
-        it = s1.upper_bound(x1[i]);
-        if(it == s1.end())
+    sort(segments, segments+n);
+
+    L[0] = segments[0].second;
+
+    for(int i=1; i<n; i++)
+        L[i] = max(L[i-1], segments[i].second);
+
+    R[n-1] = segments[n-1].second;
+    for(int i=n-2; i>=0; i--)
+        R[i] = min(R[i+1], segments[i].second);
+    
+    int ans = 0;
+
+
+    for(int i=0; i<n;i++){
+        if(i>0 && L[i-1] >= segments[i].second)
+            continue;
+        if(i<n-1 && R[i+1] <= segments[i].second)
             continue;
 
-        while(true){
-            int a = x1[i];
-            int b = x2[i];
-            int c = *it;
-            int d = x1_2_x2[*it];
-
-            bool flag = Check(a,b,c,d);
-            if(flag == false)
-                break;
-            else{
-                s1.erase(c);
-                ans_set.insert(a);
-            }
-            it = s1.upper_bound(x1[i]);
-            if(it == s1.end())
-                break;
-        }
+        ans++;
     }
-
-    set<int>::iterator it;
-    for(it = ans_set.begin(); it != ans_set.end(); it++){
-        if(s1.find(*it) != s1.end()){
-            s1.erase(*it);
-        }
-    }
-
-    int ans = s1.size();
     cout << ans;
-
     return 0;
 }
-
-/*
-        if(*it < x2[i] && x1_2_x2[*it] < x2[i]){
-            while(true){ 
-                int d = x1_2_x2[*it]; // tmp : d
-                if(d < x2[i]){ // d가 b보다 작은 경우? 
-                    ans_set.insert(*it);
-                    ans_set.insert(x1[i]);
-                    s1.erase(*it);
-                    cout << *it << " " << x1[i] << endl;
-                }
-                else
-                    break;
-                it = s1.upper_bound(x1[i]);
-                if(it == s1.end())
-                    break;
-            }
-        }
-
-        while((*it > x2[i]) &&(x1_2_x2[*it]) )
-*/
