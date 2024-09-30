235472f339a935d9ceda1bbe21119dc0beb61ae7
leebohm <102368451+leebohm@users.noreply.github.com>
2024-09-30 21:35:38 +0900

    Title: 최대 구간의 크기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/최대 구간의 크기/size-of-largest-interval.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<pair<int, int> > lines;
+
+int main() {
+
+    cin >> n;
+    
+    for(int i=0; i<n; i++){
+        int x,y;
+        cin >> x >> y;
+        lines.push_back(make_pair(x,1));
+        lines.push_back(make_pair(y,-1));
+    }
+    
+    sort(lines.begin(), lines.end());
+    int ans = 0;
+    int num = 0;
+    int tmp = 0;
+    for(int i=0; i<2*n; i++){
+        int x,v;
+        tie(x,v) = lines[i];
+        if(num >= 1){
+            int prev_x;
+            tie(prev_x, ignore) = lines[i-1];
+            tmp += (x-prev_x);
+        }
+        else if(num == 0){
+            ans = max(ans, tmp);
+        }
+        num += v;
+    }
+
+    cout << ans;
+
+    return 0;
+}
