3e441d3c8e2a2385c5635b65cfee472469b489d7
leebohm <102368451+leebohm@users.noreply.github.com>
2024-10-01 15:42:13 +0900

    Title: 연결된 정점, Time: 132ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241001/연결된 정점/connected-vertex.cpp
+++ 241001/연결된 정점/connected-vertex.cpp
@@ -1,52 +1,52 @@
 #include <iostream>
-using namespace std;
-
 #define MAX_N 100001
+
+using namespace std;
 
 int n,m;
 int uf[MAX_N];
-int cnt[MAX_N];
+int sz[MAX_N];
 
-int Find(int a){
-    if(uf[a]==a)
-        return a;
-    int ra = Find(uf[a]);
-    uf[a] = ra;
-    cnt[ra]++;
-    return uf[a];
+int Find(int x){
+    if(uf[x] == x)
+        return x;
+    return uf[x] = Find(uf[x]);
 }
-void Union(int a, int b){
-    int ra = Find(a);
-    int rb = Find(b);
-    uf[ra] = rb;
-    cnt[rb]++;
-}
-int main() {
 
+void Union(int x, int y){
+    int rx = Find(x);
+    int ry = Find(y);
+    if(rx != y){
+        uf[rx] = ry;
+        sz[ry] += sz[rx];
+    }
+}
+
+int main(){
     cin >> n >> m;
 
     for(int i=1; i<=n; i++){
         uf[i] = i;
+        sz[i] = 1;
     }
 
-    for(int i=0; i<m; i++){
-        char c;
-        int a,b;
-        cin >> c ;
-        if(c == 'x'){
+    while(m--){
+        char query;
+        cin >> query;
+
+        if(query == 'x'){
+            int a,b;
             cin >> a >> b;
             Union(a,b);
         }
-        else if(c == 'y'){
-            cin >> a;
-            int tmp = Find(a);
 
-            if(cnt[tmp]==0)
-                cout << 1 << endl;
-            else
-                cout << cnt[tmp] << endl;
+        else {
+            int a;
+            cin >> a;
+            int ra = Find(a);
+            cout << sz[ra] << endl;
         }
     }
-        
+
     return 0;
 }
