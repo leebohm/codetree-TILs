8c53b3b7f3b4c2037c240eed14c156b84c1c69d9
leebohm <102368451+leebohm@users.noreply.github.com>
2024-10-03 16:05:19 +0900

    Title: 자연수 n개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_S 2000000000
+
+using namespace std;
+
+long long s;
+
+int main(){
+    cin >> s;
+    long long left =1;
+    long long right = MAX_S;
+    long long max_num = 0;
+
+    while(left <= right){
+        long long mid = (left + right) / 2;
+        if(mid * (mid+1)/2 <= s){
+            left = mid+1;
+            max_num = max(max_num, mid);
+        }
+        else
+            right = mid -1;
+    }
+    cout << max_num;
+    return 0;
+}
