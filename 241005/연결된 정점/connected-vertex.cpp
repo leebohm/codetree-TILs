1333603ac221e32666d967f0e60b8684ce1f4275
leebohm <102368451+leebohm@users.noreply.github.com>
2024-10-05 23:20:37 +0900

    Title: 연결된 정점, Time: 130ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241005/연결된 정점/connected-vertex.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_N 100001
+
+int n,m;
+int uf[MAX_N];
+int sz[MAX_N];
+
+int Find(int x){
+    if(uf[x] == x)
+        return x;
+    return uf[x] = Find(uf[x]);
+}
+
+void Union(int x, int y){
+    int rx = Find(x);
+    int ry = Find(y);
+    uf[rx] = ry;
+    sz[ry] += sz[rx];
+}
+
+int main(){
+
+    cin >> n >> m;
+
+    for(int i=1; i<=n; i++){
+        uf[i] = i;
+        sz[i] = 1;
+    }
+
+    char c;
+    for(int i=0; i<m; i++){
+        cin >> c;
+        if(c == 'x'){
+            int a,b;
+            cin >> a >> b;
+            Union(a,b);
+        }
+        else{
+            int a;
+            cin >> a;
+            int idx  = Find(a);
+            cout << sz[idx] << endl;
+        }
+    }
+
+    return 0;
+}
