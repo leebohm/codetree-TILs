0ff3c4d25a68c986e0139b5befc519b887794c74
leebohm <102368451+leebohm@users.noreply.github.com>
2024-10-05 22:15:14 +0900

    Title: 크기 비교 3, Time: 99ms, Memory: 2MB, Status: Passed - CodeTree

--- 241005/크기 비교 3/size-comparison-3.cpp
+++ 241005/크기 비교 3/size-comparison-3.cpp
@@ -2,39 +2,41 @@
 #include <vector>
 #include <queue>
 
+#define MAX_N 32001
+
 using namespace std;
 
-#define MAX_N 32001
 int n,m;
 vector<int> edges[MAX_N];
 int indegree[MAX_N];
-queue<int> q;
+
+priority_queue<int> pq;
 
 int main(){
     cin >> n >> m;
-    for(int i=0; i<m; i++){
-        int a,b;
-        cin >> a >> b;
-        edges[a].push_back(b);
-        indegree[b]++;
-    }    
-
-    for(int i=1; i<=n; i++){
-        if(indegree[i] == 0)
-            q.push(i);
+    for(int i=1; i<=m; i++){
+        int x,y;
+        cin >> x >> y;
+        edges[x].push_back(y);
+        indegree[y]++;
     }
 
-    while(q.empty() == false){
-        int x = q.front();
-        cout << x << ' ';
-        q.pop();
+    for(int i=1; i<=n; i++)
+        if(!indegree[i])
+            pq.push(-i);
+    
+    while(!pq.empty()){
+        int x = -pq.top();
+        pq.pop();
+        cout <<  x <<" ";
+
         for(int i=0; i<(int) edges[x].size(); i++){
             int y = edges[x][i];
             indegree[y]--;
-            if(indegree[y] == 0)
-                q.push(y);
+
+            if(!indegree[y])
+                pq.push(-y);
         }
     }
-    
     return 0;
 }
