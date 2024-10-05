81f38c4939895217d13094ffe12b18b5fd3c387f
leebohm <102368451+leebohm@users.noreply.github.com>
2024-10-05 21:56:23 +0900

    Title: 친구의 키, Time: 102ms, Memory: 5MB, Status: Passed - CodeTree

---
+++ 241005/친구의 키/height-of-friends.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define MAX_N 100001
+
+int n,m;
+vector<int> edges[MAX_N];
+int indegree[MAX_N];
+queue<int> q;
+int main(){
+
+    cin >> n >> m;
+
+    for(int i=0; i<m; i++){
+        int a,b;
+        cin >> a >> b;
+        edges[a].push_back(b);
+        indegree[b]++;
+    }
+
+    for(int i=1; i<=n; i++){
+        if(indegree[i] == 0)
+            q.push(i);
+    }
+    while(q.empty()== false){
+        int x = q.front();
+        q.pop();
+        cout << x  << " ";
+        for(int i=0; i<(int) edges[x].size(); i++){
+            int y = edges[x][i];
+            indegree[y]--;
+            if(indegree[y] == 0)
+                q.push(y);
+        }
+    }
+
+    return 0;
+}
