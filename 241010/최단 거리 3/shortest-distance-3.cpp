442f8d97eb27fc26fe6b9a01a3e74a820985916f
leebohm <102368451+leebohm@users.noreply.github.com>
2024-10-10 12:16:10 +0900

    Title: 최단 거리 3, Time: 46ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 241010/최단 거리 3/shortest-distance-3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 1001
+
+int n,m;
+int a,b;
+int graph[MAX_N][MAX_N];
+int dist[MAX_N];
+bool visited[MAX_N];
+
+int main() {
+    cin >> n >> m;
+
+    for(int i=0; i<m; i++){
+        int x,y,d;
+        cin >> x >> y >> d;
+        graph[x][y] = d;
+        graph[y][x] = d;
+    }
+
+    cin >> a >> b;
+
+    for(int i=1; i<=n; i++)
+        dist[i] = (int) 1e9;
+    
+    dist[b] = 0;
+
+    for(int i=1; i<=n; i++){
+        int min_idx = -1;
+        for(int j=1; j<=n; j++){
+            if(visited[j] == true)
+                continue;
+            if(min_idx == -1 || dist[min_idx] > dist[j])
+                min_idx = j;            
+        }
+        visited[min_idx] = true;
+
+        for(int j=1; j<=n; j++){
+            if(graph[min_idx][j] == 0)
+                continue;
+            if(dist[j] > dist[min_idx] + graph[min_idx][j])
+                dist[j] = dist[min_idx] + graph[min_idx][j];
+        }
+    }
+    cout << dist[a] << "\n";
+    return 0;
+}
