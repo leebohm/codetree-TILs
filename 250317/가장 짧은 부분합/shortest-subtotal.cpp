a26653025aa0b1b671835732475b9ffa8a744652
leebohm <102368451+leebohm@users.noreply.github.com>
2025-03-17 21:30:49 +0900

    Title: 가장 짧은 부분합, Time: 23ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250317/가장 짧은 부분합/shortest-subtotal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+#define MAX_N 100005
+
+int n,s;
+int arr[MAX_N];
+
+int main() {
+    
+    cin >> n >> s;
+
+    for(int i=1; i<=n; i++)
+        cin >> arr[i];
+    
+    int ans = INT_MAX;
+
+    int j= 0;
+    int val = 0;
+    for(int i=1; i<=n; i++){
+        while(j+1 <=n && val + arr[j+1] < s ){ // 커지는 순간, j가 안 커지고 나가네 
+            val += arr[j+1];
+            j++;
+        }
+
+        ans = min(ans, j+1-i+1);
+        val -= arr[i];
+    }
+
+    cout << ans;
+    
+    return 0;
+}
+
+
